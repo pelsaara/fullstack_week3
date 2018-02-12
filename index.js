c090e65a70138af264a3cb7717978f8409fd397c
pelsaara <saara.peltomaki@gmail.com>
2018-02-12 15:46:30 +0200

    tehtava 3.8 tehty

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
 app.use(bodyParser.json())
+morgan.token('type', (req, res) => { return JSON.stringify(req.body)})
+app.use(morgan(':method :url :type :status :res[content-length] - :response-time ms'))
 
 let persons = [
     {
